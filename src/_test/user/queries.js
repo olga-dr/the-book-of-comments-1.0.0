94c6646e21079ba5fa040670556ff68a9a1c2ac7
Olga <olya.pilkevich76@gmail.com>
2024-08-22 13:35:59 -0400

    file userGetById1 was created

--- src/_test/user/queries.js
+++
@@ -1,9 +0,0 @@
-const userCreateQ = `mutation UserCreate($userInput: UserFields) {
-                       userCreate(userInput: $userInput) {
-                       firstName
-                       lastName
-                        _id
-                        }
-                      }`
-
-module.exports ={ userCreateQ}                      
